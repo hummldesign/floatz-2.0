267789e8c96a161e66bf7b35ed191058db04bc0b
hhumml <hhumml@gmail.com>
2018-05-12 16:20:10 +0200

    Adding missing events script. Fixing issues with gutter-x.

---
+++ src/main/javascript/dom/floatz.dom.events.js
@@ -0,0 +1,25 @@
+/**
+ * Event constants.
+ * Remark: Symbols can´t be used because closure compiler will change
+ *
+ * @type {string}
+ */
+
+// Element events
+export const EVENT_ANIMATIONEND = "animationend";
+export const EVENT_ANIMATIONITERATION = "animationiteration";
+export const EVENT_ANIMATIONSTART = "animationstart";
+export const EVENT_TRANSITIONEND = "transitionend";
+export const EVENT_TRANSITIONITERATION = "transitioniteration";
+export const EVENT_TRANSITIONSTART = "transitionstart";
+export const EVENT_CLICK = "click";
+export const EVENT_RESIZE = "resize";
+export const EVENT_SCROLL = "scroll";
+export const EVENT_TOUCHSTART = "touchstart";
+
+// Window event
+export const EVENT_HASHCHANGE = "hashchange";
+export const EVENT_DOMCONTENTLOADED = "DOMContentLoaded";
+export const EVENT_LOAD = "load";
+export const EVENT_BEFOREUNLOAD = "beforeunload";
+export const EVENT_UNLOAD = "unload";
