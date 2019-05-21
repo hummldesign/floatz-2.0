9c86338579e91acf204a1950e9ed08fd370a5b46
hummldesign <office@humml.eu>
2019-05-21 09:32:32 +0200

    Adding hover event.

--- src/main/javascript/dom/floatz.dom.events.js
+++ src/main/javascript/dom/floatz.dom.events.js
@@ -14,6 +14,7 @@ export const EVENT_TRANSITIONITERATION = "transitioniteration";
 export const EVENT_TRANSITIONSTART = "transitionstart";
 export const EVENT_CHANGE = "change";
 export const EVENT_CLICK = "click";
+export const EVENT_HOVER = "hover";
 export const EVENT_DOUBLECLICK = "dblclick";
 export const EVENT_RESIZE = "resize";
 export const EVENT_SCROLL = "scroll";
