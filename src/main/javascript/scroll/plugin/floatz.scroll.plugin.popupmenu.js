85f44447c5e6f89bd447b3d83e6ffc017de13c35
hummldesign <office@humml.eu>
2020-07-05 17:04:08 +0200

    Add handlers for open / close of popupmenu.

--- src/main/javascript/scroll/plugin/floatz.scroll.plugin.popupmenu.js
+++ src/main/javascript/scroll/plugin/floatz.scroll.plugin.popupmenu.js
@@ -25,187 +25,233 @@ const LOG_PREFIX = "floatz | ScrollPopupMenuPlugin | ";
  */
 export class ScrollPopupMenuPlugin extends ScrollPlugin {
 
-	/**
-	 * Constructor.
-	 *
-	 * @param options Options
-	 */
-	constructor(options = {}) {
-		super(options);
+    /**
+     * Constructor.
+     *
+     * @param options Options
+     */
+    constructor(options = {}) {
+        super(options);
 
-		// Default options
-		this.options().headerSelector = options.headerSelector  || "#header"; // FIXME
-		this.options().menuSelector = options.menuSelector || "#header .flz-nav-list"; // FIXME
-		this.options().menuClass = options.menuClass || "flz-nav-list";
-		this.options().responsiveMenuClass = options.responsiveMenuClass || "flz-nav-vmenu";
-		this.options().menuIconSelector = options.menuIconSelector || ".flz-nav-menu-icon";
-		this.options().closeMenuIcon = options.closeMenuIcon || ".flz-icon-menu-close";
-		this.options().openMenuIcon = options.openMenuIcon || ".flz-icon-menu-open";
-		this.options().slideInAnimation = options.slideInAnimation || ANIMATE_SLIDEINLEFT;
-		this.options().slideOutAnimation = options.slideOutAnimation || ANIMATE_SLIDEOUTLEFT;
-		this.options().fadeInGlassAnimation = options.fadeInGlassAnimation || ANIMATE_GLASS_FADEIN;
-		this.options().fadeOutGlassAnimation = options.fadeOutGlassAnimation || ANIMATE_GLASS_FADEOUT;
+        // Default options
+        this.options().headerSelector = options.headerSelector || "#header"; // FIXME
+        this.options().menuSelector = options.menuSelector || "#header .flz-nav-list"; // FIXME
+        this.options().menuClass = options.menuClass || "flz-nav-list";
+        this.options().responsiveMenuClass = options.responsiveMenuClass || "flz-nav-vmenu";
+        this.options().menuIconSelector = options.menuIconSelector || ".flz-nav-menu-icon";
+        this.options().closeMenuIcon = options.closeMenuIcon || ".flz-icon-menu-close";
+        this.options().openMenuIcon = options.openMenuIcon || ".flz-icon-menu-open";
+        this.options().slideInAnimation = options.slideInAnimation || ANIMATE_SLIDEINLEFT;
+        this.options().slideOutAnimation = options.slideOutAnimation || ANIMATE_SLIDEOUTLEFT;
+        this.options().fadeInGlassAnimation = options.fadeInGlassAnimation || ANIMATE_GLASS_FADEIN;
+        this.options().fadeOutGlassAnimation = options.fadeOutGlassAnimation || ANIMATE_GLASS_FADEOUT;
 
-		this._body = DOM.queryUnique(TAG_BODY);
-		this._header = DOM.queryUnique(this.options().headerSelector);
-		this._menu = DOM.queryUnique(this.options().menuSelector);
-		this._menuIcon = DOM.queryUnique(this.options().menuIconSelector);
-		this._header_z_index = null;
-		this._resizeHandler = null;
+        this._body = DOM.queryUnique(TAG_BODY);
+        this._header = DOM.queryUnique(this.options().headerSelector);
+        this._menu = DOM.queryUnique(this.options().menuSelector);
+        this._menuIcon = DOM.queryUnique(this.options().menuIconSelector);
+        this._header_z_index = null;
+        this._resizeHandler = null;
+        this._showHandlers = [];
+        this._hideHandlers = [];
 
-		// Open/close popup menu
-		this._menuIcon.addEvent(EVENT_CLICK, (e) => {
-			e.stopPropagation();
-			if (this._menuIcon.hasClass(this.options().openMenuIcon)) {
-				this.showGlass();
-				this.openMenu();
-			} else {
-				this.closeMenu();
-				this.hideGlass();
-			}
-		});
-	}
+        // Open/close popup menu
+        this._menuIcon.addEvent(EVENT_CLICK, (e) => {
+            e.stopPropagation();
+            if (this._menuIcon.hasClass(this.options().openMenuIcon)) {
+                this._show();
+            } else {
+                this._hide();
+            }
+        });
+    }
 
-	/**
-	 * @override
-	 */
-	scroller(scroller) {
-		let _scroller = super.scroller(scroller);
-		if (scroller) {
-			// Add custom event handler
-			DOM.addEvent(scroller.container(), SCROLL_EVENT_BEFORENAVGIATE, () => {
-				this.closeMenu();
-				this.hideGlass();
-			});
-		}
+    /**
+     * @override
+     */
+    scroller(scroller) {
+        let _scroller = super.scroller(scroller);
+        if (scroller) {
+            // Add custom event handler
+            DOM.addEvent(scroller.container(), SCROLL_EVENT_BEFORENAVGIATE, () => {
+                this._hide();
+            });
+        }
 
-		return _scroller;
-	}
+        return _scroller;
+    }
 
-	/**
-	 * Get body.
-	 *
-	 * @returns {DOMElement|*}
-	 */
-	body() {
-		return this._body;
-	}
+    /**
+     * Get body.
+     *
+     * @returns {DOMElement|*}
+     */
+    body() {
+        return this._body;
+    }
 
-	/**
-	 * Get menu.
-	 *
-	 * @returns {DOMElement|*}
-	 */
-	menu() {
-		return this._menu;
-	}
+    /**
+     * Get menu.
+     *
+     * @returns {DOMElement|*}
+     */
+    menu() {
+        return this._menu;
+    }
 
-	/**
-	 * Get menu icon.
-	 *
-	 * @returns {DOMElement|*}
-	 */
-	menuIcon() {
-		return this._menuIcon;
-	}
+    /**
+     * Get menu icon.
+     *
+     * @returns {DOMElement|*}
+     */
+    menuIcon() {
+        return this._menuIcon;
+    }
 
-	/**
-	 * Open menu.
-	 */
-	openMenu() {
-		this._resizeHandler = () => {
-			console.log("resize");
-			// Remove menu and glass in case that viewpoint gets larger
-			if (MediaQuery.match(MEDIA_SIZE_GTE_L)) { // FIXME
-				if (this._menuIcon.hasClass(this.options().closeMenuIcon)) {
-					this.closeMenu();
-					this.hideGlass();
-				}
-			}
-		};
-		DOM.addEvent(window, EVENT_RESIZE, this._resizeHandler);
+    /**
+     * Handler that will be called after the popup menu has been shown.
+     *
+     * @param handler Handler
+     * @returns {ScrollPopupMenuPlugin} Plugin for chaining
+     */
+    onShow(handler) {
+        this._showHandlers.push(() => {
+            handler(this)
+        });
+        return this;
+    }
 
-		// console.debug(LOG_PREFIX + "Opening menu");
-		this._header_z_index = this._header.css("z-index");
-		this._header.css("z-index", "2");
-		this.menuIcon().replaceClass(this.options().openMenuIcon, this.options().closeMenuIcon);
-		this.menu()
-			.replaceClass(this.options().menuClass, this.options().responsiveMenuClass)
-			.addClass(this.options().slideInAnimation)
-		;
-	}
+    /**
+     * Handler that will be called after the popup menu has been closed.
+     *
+     * @param handler Handler
+     * @returns {ScrollPopupMenuPlugin} Plugin for chaining
+     */
+    onHide(handler) {
+        this._hideHandlers.push(() => {
+            handler(this)
+        });
+        return this;
+    }
 
-	/**
-	 * Close menu.
-	 */
-	closeMenu() {
-		if (this.menu().hasClass(this.options().slideInAnimation)) {
-			this.menu().animate()
-				.end(() => {
-					this.menu()
-						.removeClass(this.options().slideOutAnimation)
-						.replaceClass(this.options().responsiveMenuClass, this.options().menuClass)
-					;
-					this._header.css("z-index", this._header_z_index);
-					this._header_z_index = null;
-				})
-				.trigger(() => {
-					// console.debug(LOG_PREFIX + "Closing menu");
-					this.menu().replaceClass(this.options().slideInAnimation, this.options().slideOutAnimation);
-					this.menuIcon().replaceClass(this.options().closeMenuIcon, this.options().openMenuIcon);
-				})
-			;
-		}
-	}
+    /**
+     * Open menu.
+     */
+    openMenu() {
+        this._resizeHandler = () => {
+            // Remove menu and glass in case that viewpoint gets larger
+            if (MediaQuery.match(MEDIA_SIZE_GTE_L)) { // FIXME
+                if (this._menuIcon.hasClass(this.options().closeMenuIcon)) {
+                    this._hide();
+                }
+            }
+        };
+        DOM.addEvent(window, EVENT_RESIZE, this._resizeHandler);
 
-	/**
-	 * Show glass overlay.
-	 */
-	showGlass() {
-		// console.debug(LOG_PREFIX + "Showing glass");
-		this.body()
-			.addClass(DIALOG_GLASS, this.options().fadeInGlassAnimation)
-			.addEvent(EVENT_CLICK, (e) => {
-				this._handleGlassClick(e)
-			})
-			.addEvent(EVENT_TOUCHSTART, (e) => {
-				this._handleGlassClick(e)
-			})
-		;
-	}
+        // console.debug(LOG_PREFIX + "Opening menu");
+        this._header_z_index = this._header.css("z-index");
+        this._header.css("z-index", "2");
+        this.menuIcon().replaceClass(this.options().openMenuIcon, this.options().closeMenuIcon);
+        this.menu()
+            .replaceClass(this.options().menuClass, this.options().responsiveMenuClass)
+            .addClass(this.options().slideInAnimation)
+        ;
+    }
 
-	/**
-	 * Hide glass overlay.
-	 */
-	hideGlass() {
-		if (this.body().hasClass(this.options().fadeInGlassAnimation)) {
-			this.body().animate()
-				.end(() => {
-					this.body().removeClass(this.options().fadeOutGlassAnimation, DIALOG_GLASS);
-					DOM.removeEvent(window, EVENT_RESIZE, this._resizeHandler);
-				})
-				.trigger(() => {
-					// console.debug(LOG_PREFIX + "Hiding glass");
-					this.body()
-						.replaceClass(this.options().fadeInGlassAnimation, this.options().fadeOutGlassAnimation)
-						.removeEvent(EVENT_CLICK, this._handleGlassClick)
-						.removeEvent(EVENT_TOUCHSTART, this._handleGlassClick)
+    /**
+     * Close menu.
+     */
+    closeMenu() {
+        if (this.menu().hasClass(this.options().slideInAnimation)) {
+            this.menu().animate()
+                .end(() => {
+                    this.menu()
+                        .removeClass(this.options().slideOutAnimation)
+                        .replaceClass(this.options().responsiveMenuClass, this.options().menuClass)
+                    ;
+                    this._header.css("z-index", this._header_z_index);
+                    this._header_z_index = null;
+                })
+                .trigger(() => {
+                    // console.debug(LOG_PREFIX + "Closing menu");
+                    this.menu().replaceClass(this.options().slideInAnimation, this.options().slideOutAnimation);
+                    this.menuIcon().replaceClass(this.options().closeMenuIcon, this.options().openMenuIcon);
+                })
+            ;
+        }
+    }
 
-				})
-			;
-		}
-	}
+    /**
+     * Show glass overlay.
+     */
+    showGlass() {
+        // console.debug(LOG_PREFIX + "Showing glass");
+        this.body()
+            .addClass(DIALOG_GLASS, this.options().fadeInGlassAnimation)
+            .addEvent(EVENT_CLICK, (e) => {
+                this._handleGlassClick(e)
+            })
+            .addEvent(EVENT_TOUCHSTART, (e) => {
+                this._handleGlassClick(e)
+            })
+        ;
+    }
 
-	/**
-	 * Handle click on glass.
-	 *
-	 * @param e Event
-	 * @private
-	 */
-	_handleGlassClick(e) {
-		if (e.target.tagName.toUpperCase() === TAG_BODY) {
-			this.closeMenu();
-			this.hideGlass();
-		}
-	}
+    /**
+     * Hide glass overlay.
+     */
+    hideGlass() {
+        if (this.body().hasClass(this.options().fadeInGlassAnimation)) {
+            this.body().animate()
+                .end(() => {
+                    this.body().removeClass(this.options().fadeOutGlassAnimation, DIALOG_GLASS);
+                    DOM.removeEvent(window, EVENT_RESIZE, this._resizeHandler);
+                })
+                .trigger(() => {
+                    // console.debug(LOG_PREFIX + "Hiding glass");
+                    this.body()
+                        .replaceClass(this.options().fadeInGlassAnimation, this.options().fadeOutGlassAnimation)
+                        .removeEvent(EVENT_CLICK, this._handleGlassClick)
+                        .removeEvent(EVENT_TOUCHSTART, this._handleGlassClick)
+
+                })
+            ;
+        }
+    }
+
+    /**
+     * Show glass and open menu.
+     * @private
+     */
+    _show() {
+        this.showGlass();
+        this.openMenu();
+        this._showHandlers.forEach((handler) => {
+            handler(this);
+        });
+    }
+
+    /**
+     * Close menu and hide glass.
+     * @private
+     */
+    _hide() {
+        this.closeMenu();
+        this.hideGlass();
+        this._hideHandlers.forEach((handler) => {
+            handler(this);
+        });
+    }
+
+    /**
+     * Handle click on glass.
+     *
+     * @param e Event
+     * @private
+     */
+    _handleGlassClick(e) {
+        if (e.target.tagName.toUpperCase() === TAG_BODY) {
+            this._hide();
+        }
+    }
 }
