e76ef210f32e2ffcd8325b0e86b55e3a373ccb99
hummldesign <office@humml.eu>
2025-02-09 13:57:11 +0100

    Fixing dialog size on mobile devices

--- src/demo/dialog/floatz.demo.dialog.js
+++ src/demo/dialog/floatz.demo.dialog.js
@@ -1,7 +1,5 @@
 import DOM from "../../main/javascript/dom/floatz.dom.dom.js";
 import {EVENT_CLICK, EVENT_DOMCONTENTLOADED} from "../../main/javascript/dom/floatz.dom.events.js";
-import MediaQuery, {MEDIA_LANDSCAPE} from "../../main/javascript/util/floatz.util.mediaquery.js";
-import ScreenOrientation, {ORIENTATION_LANDSCAPE, ORIENTATION_PORTRAIT} from "../../main/javascript/util/floatz.util.screenorientation.js";
 import {Dialog} from "../../main/javascript/dialog/floatz.dialog.dialog.js";
 
 let _dialog;
@@ -10,6 +8,10 @@ DOM.addEvent(window, EVENT_DOMCONTENTLOADED, () => {
     DOM.queryUnique("#openDialog").addEvent(EVENT_CLICK, (e) => {
         _dialog = new Dialog();
         _dialog.open("dialog.html", (e) => {
+            DOM.queryUnique("#dialogButtonClose").addEvent(EVENT_CLICK, (e) => {
+                console.log("close");
+                _dialog.close();
+            });
         });
     });
 });
